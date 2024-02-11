7798c495fe0a08e9295e5c0d0aeb36c057d35899
KalyanRam1234 <kalyanrammunagala@gmail.com>
2024-02-11 17:49:15 +0530

    Creating Merged CSV file

--- Task2.py
+++ Task2.py
@@ -3,3 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df1=pd.read_csv("ratings.csv")
+df2=pd.read_csv("src/Action.csv")
+# do for each genre
+dfusers=pd.read_csv("convertedusers.csv")
+
+merged_df = pd.merge(df1, df2, on='MovieID')
+df=merged_df.drop(columns=["Timestamp"])
+merge2df=pd.merge(df,dfusers[['UserID','Occupation']], on="UserID")
+
+
+print(merge2df)
+merge2df.to_csv("Task2.csv")
